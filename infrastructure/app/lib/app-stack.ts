01e5e0c7af17e07c42b2519b5a234c6ab5ee9c51
LTKSK <ltksk0714@gmail.com>
2020-10-29 09:25:38 +0900

    お試しecs設定追加

--- infrastructure/app/lib/app-stack.ts
+++ infrastructure/app/lib/app-stack.ts
@@ -1,13 +1,24 @@
 import * as cdk from '@aws-cdk/core';
-import * as s3 from "@aws-cdk/aws-s3";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
 
 export class AppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, "MyFirstBucket", { 
-      versioned: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY
+    const vpc = new ec2.Vpc(this, "MyVpc", {
+      maxAzs: 2
     });
+    const cluster = new ecs.Cluster(this, "MyCluster", { vpc })
+    const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService",{
+      cluster,
+      cpu: 256,
+      desiredCount: 2,
+      taskImageOptions: {image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample")},
+      memoryLimitMiB: 1028,
+      publicLoadBalancer: true
+    });
+    new cdk.CfnOutput(this, 'LoadBalancerDNS', {value: fargateService.loadBalancer.loadBalancerDnsName});
   }
 }
